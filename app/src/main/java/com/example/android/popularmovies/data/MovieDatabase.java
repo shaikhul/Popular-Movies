f7da7d3d4f4cb8368616df9fffa400fee87fedb1
Shaikhul Chowdhury <schowdhury@rsglab.com>
2018-06-22 23:47:37 -0400

    forgot to add MovieDatabase

---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieDatabase.java
@@ -0,0 +1,31 @@
+package com.example.android.popularmovies.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.example.android.popularmovies.models.Movie;
+
+@Database(entities = {Movie.class}, version = 2, exportSchema = false)
+public abstract class MovieDatabase extends RoomDatabase {
+    private static final String MOVIE_DB = "movie_db";
+
+    private static MovieDatabase instance;
+
+    public static MovieDatabase getInstance(Context context) {
+        if (instance != null) {
+            return  instance;
+        }
+
+        instance = Room.databaseBuilder(
+                context.getApplicationContext(),
+                MovieDatabase.class,
+                MOVIE_DB
+        ).fallbackToDestructiveMigration().build();
+
+        return instance;
+    }
+
+    public abstract MovieDao movieDao();
+}
